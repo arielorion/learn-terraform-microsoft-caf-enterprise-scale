97d214caee38f7803d8aa1a5fe57d8aaf52b0a79
arielorion <107570114+arielorion@users.noreply.github.com>
2024-01-12 21:01:34 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "West Europe"
+provider "azurerm" {
+  features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
